9e6059c90f5af32b778a00c5b002c42eeab6a5de
Tyler Wilding <xtvaser@gmail.com>
2019-01-27 17:58:36 -0500

    recording: freeze recording information to savestate regardless

--- pcsx2/SaveState.h
+++ pcsx2/SaveState.h
@@ -218,10 +218,7 @@ protected:
 
 	void deci2Freeze();
 
-#ifndef DISABLE_RECORDING
 	void InputRecordingFreeze();
-#endif
-	
 };
 
 // --------------------------------------------------------------------------------------
