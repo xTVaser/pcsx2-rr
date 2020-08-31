9451f2773deae1e74e68a3b4dc9170d5cb9bf4a1
sonicfind <52436993+sonicfind@users.noreply.github.com>
2020-08-31 11:55:28 -0500

    Update recording file copyrights from 2019 -> 2020

--- pcsx2/Recording/InputRecordingFile.h
+++ pcsx2/Recording/InputRecordingFile.h
@@ -1,5 +1,5 @@
 /*  PCSX2 - PS2 Emulator for PCs
- *  Copyright (C) 2002-2019  PCSX2 Dev Team
+ *  Copyright (C) 2002-2020  PCSX2 Dev Team
  *
  *  PCSX2 is free software: you can redistribute it and/or modify it under the terms
  *  of the GNU Lesser General Public License as published by the Free Software Found-
@@ -60,7 +60,7 @@ public:
 
 	~InputRecordingFile() { close(); }
 
-	// Closes the underlying input recording file, writing the header and 
+	// Closes the underlying input recording file, writing the header and
 	// prepares for a possible new recording to be started
 	bool close();
 	// Retrieve the input recording's filename (not the path)
