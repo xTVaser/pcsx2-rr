6b7a48148db65b61cb5d15fc5b0ef9a894a16877
kenshen112 <obarrtimothy@gmail.com>
2020-09-30 12:27:49 -0700

    Reeeeebbbbasssseeedddd

--- pcsx2/Recording/InputRecording.cpp
+++ pcsx2/Recording/InputRecording.cpp
@@ -302,24 +302,6 @@ bool InputRecording::Play(wxString fileName)
 		initialLoad = true;
 		sApp.SysExecute(g_Conf->CdvdSource);
 	}
-<<<<<<< HEAD
-	// Check author name
-	if (!g_Conf->CurrentIso.empty())
-	{
-		if (resolveGameName() != InputRecordingData.GetHeader().gameName)
-		{
-			recordingConLog(L"[REC]: Recording was possibly recorded on a different game.\n");
-		}
-	}
-	state = INPUT_RECORDING_MODE_REPLAY;
-	recordingConLog(wxString::Format(L"[REC]: Replaying movie - [%s]\n", FileName));
-	recordingConLog(wxString::Format(L"[REC]: PCSX2 Version Used: %s\n", InputRecordingData.GetHeader().emu));
-	recordingConLog(wxString::Format(L"[REC]: Recording File Version: %d\n", InputRecordingData.GetHeader().version));
-	recordingConLog(wxString::Format(L"[REC]: Associated Game Name or ISO Filename: %s\n", InputRecordingData.GetHeader().gameName));
-	recordingConLog(wxString::Format(L"[REC]: Author: %s\n", InputRecordingData.GetHeader().author));
-	recordingConLog(wxString::Format(L"[REC]: MaxFrame: %d\n", InputRecordingData.GetMaxFrame()));
-	recordingConLog(wxString::Format(L"[REC]: UndoCount: %d\n", InputRecordingData.GetUndoCount()));
-=======
 
 	// Check if the current game matches with the one used to make the original recording
 	if (!fs::is_empty(g_Conf->CurrentIso))
@@ -337,7 +319,6 @@ bool InputRecording::Play(wxString fileName)
 	recordingConLog(wxString::Format(L"[REC]: Total Frames: %d\n", inputRecordingData.GetTotalFrames()));
 	recordingConLog(wxString::Format(L"[REC]: Undo Count: %d\n", inputRecordingData.GetUndoCount()));
 	return true;
->>>>>>> 341eec743... cleaned up added WX
 }
 
 wxString InputRecording::resolveGameName()
--- pcsx2/Recording/InputRecordingFile.h
+++ pcsx2/Recording/InputRecordingFile.h
@@ -83,13 +83,12 @@ public:
 	bool WriteKeyBuffer(const uint &frame, const uint port, const uint bufIndex, const u8 &buf);
 
 private:
-<<<<<<< HEAD
 	static const int RecordingSavestateHeaderSize = sizeof(bool);
 	static const int RecordingBlockHeaderSize = 0;
 	static const int RecordingBlockDataSize = 18 * 2;
 	static const int RecordingBlockSize = RecordingBlockHeaderSize + RecordingBlockDataSize;
-	static const int RecordingSeekpointFrameMax = sizeof(InputRecordingHeader);
-	static const int RecordingSeekpointUndoCount = sizeof(InputRecordingHeader) + 4;
+	static const int RecordingSeekpointFrameMax = sizeof(InputRecordingFileHeader);
+	static const int RecordingSeekpointUndoCount = sizeof(InputRecordingFileHeader) + 4;
 	static const int RecordingSeekpointSaveState = RecordingSeekpointUndoCount + 4;
 
 	// Movie File
@@ -98,24 +97,7 @@ private:
 	long GetBlockSeekPoint(const long& frame);
 
 	// Header
-	InputRecordingHeader header;
-=======
-	static const int controllerPortsSupported = 2;
-	static const int controllerInputBytes = 18;
-	static const int inputBytesPerFrame = controllerInputBytes * controllerPortsSupported;
-	// TODO - version 2, this could be greatly simplified if everything was in the header
-	// + 4 + 4 is the totalFrame and undoCount values
-	static const int headerSize = sizeof(InputRecordingFileHeader) + 4 + 4;
-	// DEPRECATED / Slated for Removal
-	static const int recordingSavestateHeaderSize = sizeof(bool);
-	static const int seekpointTotalFrames = sizeof(InputRecordingFileHeader);
-	static const int seekpointUndoCount = sizeof(InputRecordingFileHeader) + 4;
-	static const int seekpointSaveStateHeader = seekpointUndoCount + 4;
-
 	InputRecordingFileHeader header;
-	wxString filename = "";
-	FILE* recordingFile = nullptr;
->>>>>>> 8673979e2... Clean up merge conflicts
 	InputRecordingSavestate savestate;
 
 	// An signed 32-bit frame limit is equivalent to 1.13 years of continuous 60fps footage
