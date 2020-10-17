48bc26b158b58cd7f26620bbf8df055166273667
Yuri Konotopov <ykonotopov@gnome.org>
2020-10-17 11:44:57 +0400

    Support build with system fmt
    Signed-off-by: Yuri Konotopov <ykonotopov@gnome.org>

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -233,3 +233,12 @@ if((GCC_VERSION VERSION_EQUAL "9.0" OR GCC_VERSION VERSION_GREATER "9.0") AND GC
     Even with that patch, compiling with LTO may still segfault. Use at your own risk!
     This text being in a compile log in an open issue may cause it to be closed.")
 endif()
+
+find_package(fmt "7.0.3")
+if(NOT fmt_FOUND)
+    if(EXISTS "${CMAKE_SOURCE_DIR}/3rdparty/fmt/fmt")
+        add_subdirectory(3rdparty/fmt/fmt)
+    endif()
+else()
+    message(STATUS "Found fmt: ${fmt_VERSION}")
+endif()
