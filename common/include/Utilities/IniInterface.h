c91338897be527a0e9262bced72fa9a01d21b09e
t o <finalfantasytimothy@gmail.com>
2021-01-02 19:42:30 -0500

    common: Updated path handling functions

--- common/include/Utilities/IniInterface.h
+++ common/include/Utilities/IniInterface.h
@@ -15,8 +15,9 @@
 
 #pragma once
 
-#include "Path.h"
+#include "PathUtils.h"
 #include "FixedPointTypes.h"
+#include <string>
 #include <wx/config.h>
 #include <wx/gdicmn.h>
 
@@ -53,8 +54,10 @@ public:
     virtual bool IsLoading() const = 0;
     bool IsSaving() const { return !IsLoading(); }
 
+    virtual void Entry(const std::string &var, std::string &value, const std::string defvalue = std::string()) = 0;
     virtual void Entry(const wxString &var, wxString &value, const wxString defvalue = wxString()) = 0;
     virtual void Entry(const wxString &var, wxDirName &value, const wxDirName defvalue = wxDirName(), bool isAllowRelative = false) = 0;
+    virtual void Entry(const wxString &var, fs::path &value, const fs::path defvalue = fs::path(), bool isAllowRelative = false) = 0;
     virtual void Entry(const wxString &var, wxFileName &value, const wxFileName defvalue = wxFileName(), bool isAllowRelative = false) = 0;
     virtual void Entry(const wxString &var, int &value, const int defvalue = 0) = 0;
     virtual void Entry(const wxString &var, uint &value, const uint defvalue = 0) = 0;
@@ -117,8 +120,10 @@ public:
 
     bool IsLoading() const { return true; }
 
+    void Entry(const std::string &var, std::string &value, const std::string defvalue = std::string());
     void Entry(const wxString &var, wxString &value, const wxString defvalue = wxEmptyString);
     void Entry(const wxString &var, wxDirName &value, const wxDirName defvalue = wxDirName(), bool isAllowRelative = false);
+    void Entry(const wxString &var, fs::path &value, const fs::path defvalue = fs::path(), bool isAllowRelative = false);
     void Entry(const wxString &var, wxFileName &value, const wxFileName defvalue = wxFileName(), bool isAllowRelative = false);
     void Entry(const wxString &var, int &value, const int defvalue = 0);
     void Entry(const wxString &var, uint &value, const uint defvalue = 0);
@@ -155,8 +160,10 @@ public:
 
     bool IsLoading() const { return false; }
 
+    void Entry(const std::string &var, std::string &value, const std::string defvalue = std::string());
     void Entry(const wxString &var, wxString &value, const wxString defvalue = wxString());
     void Entry(const wxString &var, wxDirName &value, const wxDirName defvalue = wxDirName(), bool isAllowRelative = false);
+    void Entry(const wxString &var, fs::path &value, const fs::path defvalue = fs::path(), bool isAllowRelative = false);
     void Entry(const wxString &var, wxFileName &value, const wxFileName defvalue = wxFileName(), bool isAllowRelative = false);
     void Entry(const wxString &var, int &value, const int defvalue = 0);
     void Entry(const wxString &var, uint &value, const uint defvalue = 0);
