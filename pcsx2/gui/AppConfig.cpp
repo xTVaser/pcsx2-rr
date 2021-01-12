356ba4f1244a2ac54db03316dd9658e9c610eb49
t o <finalfantasytimothy@gmail.com>
2021-01-12 12:56:06 -0700

    AppConfig: Change Bios to use absolute path for folder location. Change snapshots to snaps

--- pcsx2/gui/AppConfig.cpp
+++ pcsx2/gui/AppConfig.cpp
@@ -183,7 +183,7 @@ namespace PathDefs
 
 	fs::path GetSnapshots()
 	{
-		return (GetDocuments() / "snapshots").make_preferred();
+		return (GetDocuments() / "snaps").make_preferred();
 	}
 
 	fs::path GetBios()
@@ -764,7 +764,7 @@ void AppConfig::FolderOptions::LoadSave( IniInterface& ini )
 	 //  --> on load, these relative paths will be expanded relative to the exe folder.
 	bool rel = ( ini.IsLoading() || IsPortable() );
 	
-	Bios = Path::isPortable(Bios, IsPortable());
+	//Bios = Path::isPortable(Bios, IsPortable());
 	Snapshots = Path::isPortable(Snapshots, IsPortable());
 	Savestates = Path::isPortable(Savestates, IsPortable());
 	MemoryCards = Path::isPortable(MemoryCards, IsPortable());
