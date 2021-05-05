fbbff343e3cb84e674e1bd438eec357e4b504b5e
Tyler Wilding <xtvaser@gmail.com>
2021-05-04 23:05:44 -0400

    test

--- .github/workflows/scripts/validation/lint-gamedb/lint-gamedb.py
+++ .github/workflows/scripts/validation/lint-gamedb/lint-gamedb.py
@@ -65,10 +65,98 @@ def validate_int_option(serial, key, value, low, high):
             )
         )
 
-
 def validate_valid_options(serial, key, value, allowed_values):
     if value not in allowed_values:
         issue_list.append("[{}]: Invalid '{}' option [{}]".format(serial, key, value))
+
+def validate_game_option(serial, key, value): # noqa: C901
+    if key not in allowed_game_options:
+        issue_list.append("[{}]: Invalid option [{}]".format(serial, key))
+
+    # SIMPLE METADATA VALIDATION
+    if key in ["name", "region"]:
+        validate_string_option(serial, key, value)
+
+    if key == "compat":
+        validate_int_option(serial, key, value, 0, 6)
+
+    # ROUND MODE VALIDATION
+    if key == "roundModes" and isinstance(value, dict):
+        for roundMode, roundValue in value.items():
+            validate_valid_options(serial, key, roundMode, allowed_round_modes)
+            validate_int_option(serial, key, roundValue, 0, 3)
+    elif key == "roundModes":
+        issue_list.append(
+            "[{}]: 'roundModes' must be a valid object".format(serial)
+        )
+
+    # CLAMP MODE VALIDATION
+    if key == "clampModes" and isinstance(value, dict):
+        for clampMode, clampValue in value.items():
+            validate_valid_options(serial, key, clampMode, allowed_clamp_modes)
+            validate_int_option(serial, key, clampValue, 0, 3)
+    elif key == "clampModes":
+        issue_list.append(
+            "[{}]: 'clampModes' must be a valid object".format(serial)
+        )
+
+    # GAME FIX VALIDATION
+    if key == "gameFixes" and isinstance(value, list):
+        for gamefix in value:
+            validate_valid_options(serial, key, gamefix, allowed_game_fixes)
+    elif key == "gameFixes":
+        issue_list.append(
+            "[{}]: 'gameFixes' must be a list of valid gameFixes".format(serial)
+        )
+
+    # SPEEDHACKS VALIDATION
+    if key == "speedHacks" and isinstance(value, dict):
+        for speedhack, speedhackValue in value.items():
+            validate_valid_options(serial, key, speedhack, allowed_speed_hacks)
+            validate_int_option(serial, speedhack, speedhackValue, 0, 1)
+    elif key == "speedHacks":
+        issue_list.append(
+            "[{}]: 'speedHacks' must be a valid object".format(serial)
+        )
+
+    # MEM CARD VALIDAITON
+    if key == "memcardFilters" and not all(
+        isinstance(memcardFilter, str) for memcardFilter in value
+    ):
+        issue_list.append(
+            "[{}]: 'memcardFilters' must be a list of strings".format(serial)
+        )
+
+    # PATCH VALIDATION
+    if key == "patches" and isinstance(value, dict):
+        for crc, patch in value.items():
+            if crc != "default" and not is_hex_number(str(crc)):
+                issue_list.append(
+                    "[{}]: Patches must either be key'd with 'default' or a valid CRC-32 Hex String".format(
+                        serial
+                    )
+                )
+                continue
+            for patchOption, optionValue in patch.items():
+                validate_valid_options(
+                    serial, key, patchOption, allowed_patch_options
+                )
+                if key == "author":
+                    validate_string_option(serial, patchOption, optionValue)
+                if key == "content" and not all(
+                    isinstance(patchLine, str) for patchLine in optionValue
+                ):
+                    issue_list.append(
+                        "[{}]: Patch 'content' must be a list of strings".format(
+                            serial
+                        )
+                    )
+    elif key == "patches":
+        issue_list.append(
+            "[{}]: 'patches' must be valid mapping of CRC32 -> Patch Objects".format(
+                serial
+            )
+        )
 
 
 print("Opening {}...".format(file_path))
@@ -85,7 +173,7 @@ with open(file_path) as f:
 
     print("File loaded successfully, validating schema...")
     progress_counter = 0
-    for serial, game_options in gamedb.items(): # noqa: MC0001
+    for serial, game_options in gamedb.items():
         progress_counter = progress_counter + 1
         if progress_counter % 500 == 0 or progress_counter >= len(gamedb.items()):
             print(
@@ -100,94 +188,9 @@ with open(file_path) as f:
         if not "region" in game_options.keys():
             issue_list.append("[{}]: 'region' is a required value".format(serial))
 
+        # Check the options
         for key, value in game_options.items():
-            if key not in allowed_game_options:
-                issue_list.append("[{}]: Invalid option [{}]".format(serial, key))
-
-            # SIMPLE METADATA VALIDATION
-            if key in ["name", "region"]:
-                validate_string_option(serial, key, value)
-
-            if key == "compat":
-                validate_int_option(serial, key, value, 0, 6)
-
-            # ROUND MODE VALIDATION
-            if key == "roundModes" and isinstance(value, dict):
-                for roundMode, roundValue in game_options["roundModes"].items():
-                    validate_valid_options(serial, key, roundMode, allowed_round_modes)
-                    validate_int_option(serial, key, roundValue, 0, 3)
-            elif key == "roundModes":
-                issue_list.append(
-                    "[{}]: 'roundModes' must be a valid object".format(serial)
-                )
-
-            # CLAMP MODE VALIDATION
-            if key == "clampModes" and isinstance(value, dict):
-                for clampMode, clampValue in game_options["clampModes"].items():
-                    validate_valid_options(serial, key, clampMode, allowed_clamp_modes)
-                    validate_int_option(serial, key, clampValue, 0, 3)
-            elif key == "clampModes":
-                issue_list.append(
-                    "[{}]: 'clampModes' must be a valid object".format(serial)
-                )
-
-            # GAME FIX VALIDATION
-            if key == "gameFixes" and isinstance(value, list):
-                for gamefix in game_options["gameFixes"]:
-                    validate_valid_options(serial, key, gamefix, allowed_game_fixes)
-            elif key == "gameFixes":
-                issue_list.append(
-                    "[{}]: 'gameFixes' must be a list of valid gameFixes".format(serial)
-                )
-
-            # SPEEDHACKS VALIDATION
-            if key == "speedHacks" and isinstance(value, dict):
-                for speedhack, speedhackValue in game_options["speedHacks"].items():
-                    validate_valid_options(serial, key, speedhack, allowed_speed_hacks)
-                    validate_int_option(serial, speedhack, speedhackValue, 0, 1)
-            elif key == "speedHacks":
-                issue_list.append(
-                    "[{}]: 'speedHacks' must be a valid object".format(serial)
-                )
-
-            # MEM CARD VALIDAITON
-            if key == "memcardFilters" and not all(
-                isinstance(memcardFilter, str) for memcardFilter in value
-            ):
-                issue_list.append(
-                    "[{}]: 'memcardFilters' must be a list of strings".format(serial)
-                )
-
-            # PATCH VALIDATION
-            if key == "patches" and isinstance(value, dict):
-                for crc, patch in game_options["patches"].items():
-                    if crc != "default" and not is_hex_number(str(crc)):
-                        issue_list.append(
-                            "[{}]: Patches must either be key'd with 'default' or a valid CRC-32 Hex String".format(
-                                serial
-                            )
-                        )
-                        continue
-                    for patchOption, optionValue in patch.items():
-                        validate_valid_options(
-                            serial, key, patchOption, allowed_patch_options
-                        )
-                        if key == "author":
-                            validate_string_option(serial, patchOption, optionValue)
-                        if key == "content" and not all(
-                            isinstance(patchLine, str) for patchLine in optionValue
-                        ):
-                            issue_list.append(
-                                "[{}]: Patch 'content' must be a list of strings".format(
-                                    serial
-                                )
-                            )
-            elif key == "patches":
-                issue_list.append(
-                    "[{}]: 'patches' must be valid mapping of CRC32 -> Patch Objects".format(
-                        serial
-                    )
-                )
+            validate_game_option(serial, key, value)
 
 if len(issue_list) > 0:
     print("Issues found during validation:")
